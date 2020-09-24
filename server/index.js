0a03a0fb6ffa16901135e7a1969446c2f53b1072
HostX <51702129+HostXX@users.noreply.github.com>
2020-09-24 02:47:31 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,16 +3,14 @@ const express = require("express");
 const app = express(); // create express app
 
 // add middlewares
-app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("build"));
+app.use(express.static(path.join(__dirname, "..", "build")));
+
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
-app.get('*', (req,res)=> {
-    res.sendFile(path.resolve())
-})
 
 // start express server on port 5000
 app.listen(5000, () => {

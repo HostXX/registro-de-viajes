444d203ec81c23c8b47abeee2be58f7b46544ddf
HostX <51702129+HostXX@users.noreply.github.com>
2020-09-24 02:46:19 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express(); // create express app
 
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "build")));
-app.use(express.static("public"));
+app.use(express.static("build"));
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));

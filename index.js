5cc3a9f19e2802ed4adf9ddde8d69c0ab467d03a
Carlos Garcia <carlosmauriciogarcia13@gmail.com>
2020-09-24 01:56:09 -0400

    dep

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const app = express(); // create express app
 
 // add middlewares
 app.use(express.static(path.join(__dirname, "build")));
-app.use(express.static("public"));
+app.use(express.static("build"));
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname,"build", "index.html"));
 });
 
-app.get('*', (req,res)=> {
-    res.sendFile(path.resolve())
-})
+// app.get('*', (req,res)=> {
+//     res.sendFile(path.resolve())
+// })
 
 // start express server on port 5000
 app.listen(5000, () => {
